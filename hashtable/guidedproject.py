32c9ed7a29a99fbc112df452afd219d3ce0ac24b
Josue Rodriguez <smexyjosue@gmail.com>
2020-08-03 14:52:51 -0700

    adds additional file to practice hashing

---
+++ hashtable/guidedproject.py
@@ -0,0 +1,36 @@
+my_list = ["hi", "how", "are", "you", "hello", "world"]
+
+
+def my_hash(l):
+    string_bytes = l.encode()
+    total = 0
+    for letter in string_bytes:
+        total += letter
+
+    return total
+
+
+new_list = [None] * 8
+
+# How to put / insert a value to a key
+hello_hash = my_hash("hello")
+hello_index = hello_hash % len(new_list)
+print(hello_index)
+new_list[hello_index] = 'hello world'
+print(new_list)
+print(hello_index)
+print(hello_hash)
+
+
+hashed_world = my_hash('world')
+world_index = hashed_world % len(new_list)
+print(world_index)
+new_list[world_index] = 'howdy world'
+print(new_list)
+print(hello_index)
+print(hello_hash)
+
+# How to search for a value
+hashed_string = my_hash('hello')
+our_index = hashed_string % len(new_list)
+print(new_list[our_index])
