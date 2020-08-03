0085151872cdbf0e5c1ae0ab959041dcd1aced00
Josue Rodriguez <smexyjosue@gmail.com>
2020-08-03 15:05:26 -0700

    tests a good hashing function named DJB2

--- hashtable/guidedproject.py
+++ hashtable/guidedproject.py
@@ -15,22 +15,32 @@ new_list = [None] * 8
 # How to put / insert a value to a key
 hello_hash = my_hash("hello")
 hello_index = hello_hash % len(new_list)
-print(hello_index)
+# print(hello_index)
 new_list[hello_index] = 'hello world'
-print(new_list)
-print(hello_index)
-print(hello_hash)
+# print(new_list)
+# print(hello_index)
+# print(hello_hash)
 
 
 hashed_world = my_hash('world')
 world_index = hashed_world % len(new_list)
-print(world_index)
+# print(world_index)
 new_list[world_index] = 'howdy world'
-print(new_list)
-print(hello_index)
-print(hello_hash)
+# print(new_list)
+# print(hello_index)
+# print(hello_hash)
 
 # How to search for a value
 hashed_string = my_hash('hello')
 our_index = hashed_string % len(new_list)
-print(new_list[our_index])
+# print(new_list[our_index])
+
+
+def djb2(key):
+    hash = 5381
+    for c in key:
+        hash = (hash * 33) + ord(c)
+    return hash
+
+
+print(djb2('hello world'))
